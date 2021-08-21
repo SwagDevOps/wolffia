1c33fcf4fde0fd8ebf613da9b2b36e2a82aeae43
Dimitri Arrigoni <dimitri@arrigoni.me>
2021-08-21 22:46:50 +0200

    fix(mixins): env missing header comment

--- lib/wolffia/mixins/env.rb
+++ lib/wolffia/mixins/env.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
+# Copyright (C) 2019-2021 Dimitri Arrigoni <dimitri@arrigoni.me>
+# License GPLv3+: GNU GPL version 3 or later
+# <http://www.gnu.org/licenses/gpl.html>.
 # This is free software: you are free to change and redistribute it.
 # There is NO WARRANTY, to the extent permitted by law.
 
