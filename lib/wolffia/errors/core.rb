2983b4fa4e19a6ce74a8b232bb9bad4a5725bd33
Dimitri Arrigoni <dimitri@arrigoni.me>
2021-08-21 18:34:45 +0200

    feat(errors): introduce core/missing_injector_error

---
+++ lib/wolffia/errors/core.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# Copyright (C) 2019-2021 Dimitri Arrigoni <dimitri@arrigoni.me>
+# License GPLv3+: GNU GPL version 3 or later
+# <http://www.gnu.org/licenses/gpl.html>.
+# This is free software: you are free to change and redistribute it.
+# There is NO WARRANTY, to the extent permitted by law.
+
+require_relative '../errors'
+
+# Namespace module
+module Wolffia::Errors::Core
+  # rubocop:disable Lint/EmptyBlock
+  include(Wolffia::Autoloaded).autoloaded {}
+  # rubocop:enable Lint/EmptyBlock
+end
