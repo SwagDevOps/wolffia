cbc7a71f4dbf45d9ac013b7c3d6dc52343ca36a6
Dimitri Arrigoni <dimitri@arrigoni.me>
2021-09-30 08:47:12 +0200

    spec(app): introduce app sham with samples

---
+++ spec/sham/app.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+autoload(:YAML, 'yaml')
+
+paths_builder = lambda do
+  SAMPLES_PATH.join('app').then do |path|
+    path.entries
+        .sort
+        .reject { |v| %w[. ..].include?(v.to_path) }
+        .map { |fp| path.join(fp).realpath }
+        .keep_if(&:directory?)
+        .map { |dirpath| [dirpath.basename.to_s.to_sym, dirpath] }
+        .to_h
+  end
+end
+
+apps_builder = lambda do |paths|
+  paths.transform_values do |path|
+    -> { Class.new(::Wolffia).call(path: path) }
+  end
+end
+
+expectations_builder = lambda do |paths|
+  paths.transform_values do |dir|
+    YAML.safe_load(dir.join('expectations.yml').read).to_h.transform_keys(&:to_sym).then do |parsed|
+      ::Struct.new(*parsed.keys).new(*parsed.values)
+    end
+  end
+end
+
+# result --------------------------------------------------------------
+
+paths_builder.call.then do |paths|
+  {
+    paths: paths,
+    builders: apps_builder.call(paths),
+    expectations: expectations_builder.call(paths),
+    valid_keys: paths.keys.keep_if { |v| v.to_s.match(/^valid_.+$/) }
+  }
+end
