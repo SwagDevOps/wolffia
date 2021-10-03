8660caa2a731c53dc38008e959991a09ee618ab4
Dimitri Arrigoni <dimitri@arrigoni.me>
2021-10-03 12:21:32 +0200

    spec(dotenv): add some tests

--- spec/wolffia/dotenv_spec.rb
+++ spec/wolffia/dotenv_spec.rb
@@ -2,6 +2,36 @@
 
 autoload(:Pathname, 'pathname')
 
+# methods signatures ------------------------------------------------
+describe Wolffia::Dotenv, :'wolffia/dotenv' do
+  let(:subject) { described_class.allocate }
+  :new.tap do |method|
+    it { expect(described_class).to respond_to(method).with(0).arguments.with_keywords(:path) }
+    it { expect(described_class).to respond_to(method).with(0).arguments.with_keywords(:filename) }
+  end
+
+  it { expect(subject).to respond_to(:call).with(0).arguments }
+  it { expect(subject).to respond_to(:to_s).with(0).arguments }
+  it { expect(subject).to respond_to(:to_path).with(0).arguments }
+end
+
+# public methods ----------------------------------------------------
+describe Wolffia::Dotenv, :'wolffia/dotenv' do
+  sham(:app).paths.each do |_, path|
+    [:to_s, :to_path].each do |method|
+      context "##{method}" do
+        let(:path) { Pathname.new(path) }
+        let(:filpath) { path.join('.env') }
+        let(:subject) { described_class.new(path: path) }
+
+        it { expect(subject.public_send(method)).to be_a(String) }
+        it { expect(subject.public_send(method)).to eq(filpath.to_s) }
+      end
+    end
+  end
+end
+
+# internal state ----------------------------------------------------
 describe Wolffia::Dotenv, :'wolffia/dotenv' do
   sham(:app).paths.each do |_, path|
     context '#path' do
