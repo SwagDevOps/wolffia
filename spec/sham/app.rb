a48cf6e809831b56114cc03dc0e5c318f6539bf7
Dimitri Arrigoni <dimitri@arrigoni.me>
2021-10-03 10:36:27 +0200

    spec(sham): add builder can use given env (default: empty)

--- spec/sham/app.rb
+++ spec/sham/app.rb
@@ -2,6 +2,7 @@
 
 autoload(:YAML, 'yaml')
 
+# List paths --------------------------------------------------------
 paths_builder = lambda do
   SAMPLES_PATH.join('app').then do |path|
     path.entries
@@ -14,12 +15,18 @@ paths_builder = lambda do
   end
 end
 
+# Build an app form a path, with an empty env -----------------------
 apps_builder = lambda do |paths|
   paths.transform_values do |path|
-    -> { Class.new(::Wolffia).call(path: path) }
+    lambda do |env: {}|
+      Class.new(::Wolffia).then do |klass|
+        with_env(env) { klass.call(path: path) }
+      end
+    end
   end
 end
 
+# Retrieve exepectations --------------------------------------------
 expectations_builder = lambda do |paths|
   paths.transform_values do |dir|
     YAML.safe_load(dir.join('expectations.yml').read).to_h.transform_keys(&:to_sym).then do |parsed|
@@ -35,6 +42,7 @@ paths_builder.call.then do |paths|
     paths: paths,
     builders: apps_builder.call(paths),
     expectations: expectations_builder.call(paths),
-    valid_env_keys: paths.keys.keep_if { |v| v.to_s.match(/^valid_env_.+$/) }
+    valid_env_keys: paths.keys.keep_if { |v| v.to_s.match(/^valid_env_.+$/) },
+    invalid_env_keys: paths.keys.keep_if { |v| v.to_s.match(/^invalid_env_.+$/) },
   }
 end
