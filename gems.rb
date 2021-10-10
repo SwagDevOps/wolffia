f985e66f6f014fd5e3b2b9a0e3cc709e16384fbf
Dimitri Arrigoni <dimitri@arrigoni.me>
2021-10-10 02:12:44 +0200

    feat(gems): add some development gems + set version for rbminivents

--- gems.rb
+++ gems.rb
@@ -23,7 +23,7 @@ group :default do
   gem 'dry-container', '~> 0.8'
   gem 'hanami-router', '~> 1.3'
   gem 'kamaze-version', '~> 1.0'
-  gem 'rbminivents'
+  gem 'rbminivents', '~> 0.2'
   gem 'ruby_parser', '~> 3.17'
   gem 'stibium-bundled', '~> 0.0', '>= 0.0.4'
   gem 'sys-proc', '~> 1.1'
@@ -38,12 +38,16 @@ group :development do
   # repl ---------------------------------
   gem 'interesting_methods', '~> 0.1'
   gem 'pry', '~> 0.12'
-  # web ----------------------------------
+  # middlewares --------------------------
   gem 'better_errors', '~> 2.9'
   gem 'binding_of_caller', '~> 1.0'
+  gem 'rack-contrib', '~> 2.3'
   gem 'rack-rewrite', '~> 1.5'
+  # web ----------------------------------
+  gem 'ruby-prof', '~> 1.4'
   gem 'thin', '~> 1.8'
   gem 'webrick', '~> 1.6'
+
   github 'ksylvest/rhino', { branch: 'master' }
   github 'alexch/rerun', { branch: 'master' }
   github 'sqm/http_router', { branch: 'master' }
