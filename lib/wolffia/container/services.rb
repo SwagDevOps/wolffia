3d14f069923f7361287a1b040c13b72a20626226
Dimitri Arrigoni <dimitri@arrigoni.me>
2021-11-01 17:04:27 +0100

    feat(services): refactor + add gem.loaded_specs

--- lib/wolffia/container/services.rb
+++ lib/wolffia/container/services.rb
@@ -4,12 +4,20 @@ register(:json, memoize: true) do
   (require 'json').yield_self { JSON }
 end
 
+# logger ----------------------------------------------------------------------
 register(:logger, memoize: true) do
   ::Wolffia::Logger.new do |config|
     config.name = :web
   end
 end
 
+# paths -----------------------------------------------------------------------
+volatile_get(:paths).tap do |paths|
+  register('app.paths', memoize: true) { paths }
+  paths.each { |name, path| self["app.paths.#{name}_path"] = path }
+end
+
+# app -------------------------------------------------------------------------
 volatile_get(:environment).tap do |environment|
   register('app.env', memoize: true) { environment }
 end
@@ -18,21 +26,27 @@ volatile_get(:'app.instance').tap do |app|
   register('app.instance', memoize: true) { app }
 end
 
-volatile_get(:paths).tap do |paths|
-  register('app.paths', memoize: true) { paths }
-  paths.each { |name, path| self["app.paths.#{name}_path"] = path }
-end
-
 volatile_get(:settings_params).tap do |params|
   register('app.settings', memoize: true) { ::Wolffia::Config.new(*params).settings }
 end
 
+# router ----------------------------------------------------------------------
 volatile_get(:router_options).tap do |options|
   register(:'http.router', memoize: true) do
     ::Wolffia::HTTP::Router.new(**options).register(container: self)
   end
 end
 
+# cli -------------------------------------------------------------------------
 volatile_get(:commands).tap do |commands|
   register(:cli, memoize: true) { ::Wolffia::Cli.build(commands) }
+end
+
+# rubygems --------------------------------------------------------------------
+if defined?(:Gem)
+  if Gem.loaded_specs.is_a?(::Hash) # rubocop:disable Style/SoleNestedConditional
+    register(:'gem.loaded_specs', memoize: true) do
+      Gem.loaded_specs.transform_keys { |key| key.to_s.freeze }
+    end
+  end
 end
